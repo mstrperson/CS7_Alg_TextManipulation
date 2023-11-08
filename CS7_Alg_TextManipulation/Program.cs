b88c779b8e9739816f6386894bf3cbbfd5e7561f
Jason Cox <coxj@vt.edu>
2023-11-08 11:30:06 -0500

    Initial Commit

---
+++ CS7_Alg_TextManipulation/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#region Program
+Console.WriteLine("Enter a secret Message");
+var message = Console.ReadLine()!;
+var encrypted = EncryptOrDecrypt(message, "my_password");
+Console.WriteLine(encrypted);
+#endregion
+
+#region Methods
+
+string EncryptOrDecrypt(string message, string password)
+{
+    string output = message;
+    
+    // TODO:  Implement Encryption using your Password and the XOR operator
+    
+    return output;
+}
+
+#endregion
